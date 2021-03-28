332d5a297c71983d86b6d31c30d33b26796000f2
Yangkai Du <yangkai@YangkaideMacBook-Air.local>
2021-03-28 15:42:28 +0800

    add XLNet support

--- src/main_xlnet.py
+++ src/main_xlnet.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from copy import deepcopy
 from transformers import XLNetModel, XLNetConfig, XLNetForSequenceClassification
-from data_xlnet import collate_fn, collate_fn_mix
 from data import multiLabelDataset
+from data_xlnet import collate_fn, collate_fn_mix
 from torch.utils.data import TensorDataset, DataLoader
 import argparse
 import random
@@ -13,15 +13,21 @@ import os
 import tqdm
 from opt import OpenAIAdam
 import tqdm
-
+from model import scl_model_Xlnet
 
 class Recoder_multi():
     def __init__(self,args):
         self.args = args
+        self.ce_loss_x = []
+        self.ce_loss_s = []
+        self.scl_loss = []
         self.loss = []
         self.acc = []
         self.step = []
-    def log_train(self,loss):
+    def log_train(self,ce_loss_x, ce_loss_s, scl_loss, loss):
+        self.ce_loss_x.append(ce_loss_x.item())
+        self.ce_loss_s.append(ce_loss_s.item())
+        self.scl_loss.append(scl_loss.item())
         self.loss.append(loss.item())
     
     def log_test(self,acc,step):
@@ -34,6 +40,9 @@ class Recoder_multi():
         print("===================================")
         print("step: ",step)
         print("loss: ",sum(self.loss[st:ed])/self.args.log_step)
+        print("ce_loss_x: ",sum(self.ce_loss_x[st:ed])/self.args.log_step)
+        print("ce_loss_s: ",sum(self.ce_loss_s[st:ed])/self.args.log_step)
+        print("scl_loss: ",sum(self.scl_loss[st:ed])/self.args.log_step)
     
 
 
@@ -51,7 +60,7 @@ def evaluate_model(model, test_loader, recoder, step):
             x_ids, s_mix_ids, y_a, y_b = batch
             seq_ids = x_ids.to(device)
             labels = y_a.to(device)
-            logits = model(input_ids=seq_ids,labels=labels)[1]
+            logits = model.predict(seq_ids)
 
             prediction = torch.argmax(logits, dim = 1)
             correct += (prediction == labels).sum().item()
@@ -62,37 +71,54 @@ def evaluate_model(model, test_loader, recoder, step):
 
     recoder.log_test(acc,step)
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--steps', type=int, default=10000)
 parser.add_argument("--seed",type=int,default=41)
 parser.add_argument("--gpu_ids",type=int,default=0)
+parser.add_argument("--with_mix", action='store_true')
 parser.add_argument("--batch_size", type=int, default=8)
 parser.add_argument("--eval_batch_size", type=int, default=16)
+parser.add_argument("--num_accum",type=int,default=1)
+parser.add_argument("--max_len",type=int, default=200)
 parser.add_argument('--lr', type=float, default=1e-5)
 parser.add_argument('--clip',type=float,default=1)
+parser.add_argument("--lambd",type=float,default=0.8)
 parser.add_argument('--log_step',type=int,default=100)
 parser.add_argument('--log_dir',type=str,default="finetune_log.pkl")
 
-parser.add_argument('--dataset',type=str,default="amazon")
+parser.add_argument('--dataset',type=str,default="amazon_2")
 parser.add_argument('--train_num',type=float,default=80)
+parser.add_argument('--with_summary',action='store_true')
 
 args = parser.parse_args()
+
+
+if args.seed is not None:
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    cudnn.deterministic = True
 
 # Make Dataset
 train_dataset = multiLabelDataset(dataset_name = args.dataset,max_num=args.train_num,seed=args.seed,split="train")
 test_dataset = multiLabelDataset(dataset_name = args.dataset,max_num=10000,seed=args.seed,split="test")
 
-
-
-my_collect = collate_fn
+if args.with_mix:
+    my_collect = collate_fn_mix
+else:
+    my_collect = collate_fn
 train_loader = DataLoader(train_dataset, num_workers=2, batch_size=args.batch_size, shuffle=True, collate_fn = my_collect)
 test_loader = DataLoader(test_dataset, num_workers=2, batch_size=args.eval_batch_size,shuffle=False,collate_fn=my_collect)
 
 # ##make model
 device = torch.device(args.gpu_ids)
 config = XLNetConfig.from_pretrained("xlnet-base-cased")
-config.num_labels = args.num_labels
-model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased",config=config)
+config.num_labels = 5
+if args.dataset is "ag_news":
+  config.num_labels = 4
+pretrained_model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased",config=config)
+model = scl_model_Xlnet(config,device,pretrained_model,with_semi=args.with_mix,with_sum = args.with_summary)
+
 ##make optimizer
 optimizer = OpenAIAdam(model.parameters(),
                                   lr=args.lr,
@@ -115,9 +141,6 @@ bar = tqdm.tqdm(total=args.steps)
 bar.update(0)
 best_acc = 0
 recoder = Recoder_multi(args)
-loss_mask_str = args.loss_mask.split(',')
-loss_mask = [float(i) for i in loss_mask_str]
-print(loss_mask)
 
 best_loss = float('inf')
 count = 0
@@ -125,17 +148,21 @@ begin_eval = False
 while(step < args.steps):
     model.train()
     for batch in train_loader:
-        x_ids, s_mix_ids, y_a, y_b = batch
-        seq_ids = x_ids.to(device)
-        labels = y_a.to(device)
-        loss = model(input_ids=seq_ids,labels=labels)[0]
-        # print(ce_loss_x, ce_loss_s, scl_loss, ucl_loss)
+        # optimizer.zero_grad()
+        # print(batch)
+        ce_loss_x, ce_loss_s, scl_loss = model(batch)
+        ce_loss = (ce_loss_x + ce_loss_s)/2
+        if not args.with_summary:
+            ce_loss = ce_loss_x
+        loss = args.lambd * ce_loss+ (1-args.lambd) * scl_loss
+
+        # print(ce_loss_x, ce_loss_s, scl_loss)
         loss.backward()
 
         count += 1
         if (count % args.num_accum == 0):
             optimizer.step()
-            recoder.log_train(ce_loss_x, ce_loss_s, scl_loss,ucl_loss,loss)
+            recoder.log_train(ce_loss_x, ce_loss_s, scl_loss,loss)
             step += 1
             optimizer.zero_grad()
 
@@ -147,12 +174,14 @@ while(step < args.steps):
 
             if (step % 10 == 0):
                 bar.update(10)
+
+        # step += 1
         
         
         if begin_eval:
             recoder.meter(step)
             evaluate_model(model,test_loader,recoder,step)
-            torch.save(recoder, "../"+args.log_dir)
+            torch.save(recoder, "../" + args.log_dir)
 
             model.train()
             begin_eval = False
